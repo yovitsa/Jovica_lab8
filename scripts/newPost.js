44b0f53af065463cd1888b12566ffc63fc1ffa84
yovitsa <134582277+yovitsa@users.noreply.github.com>
2024-03-31 11:04:37 -0700

    First commit

---
+++ scripts/newPost.js
@@ -0,0 +1,23 @@
+/** COMPLETE THIS PART */
+async function addPost(post) {
+    /**
+     * 1. Get the list posts from local storage
+     *      if local storage is empty, initialize the list of posts to an empty array
+     * 2. Add the new post at the front of list
+     * 3. Save the list posts back to local storage
+     */
+
+    // Your code here
+      let posts = JSON.parse(localStorage.getItem('posts')) || [];
+    posts.unshift(post); // Add new post at the start of the array
+    localStorage.setItem('posts', JSON.stringify(posts));
+}
+
+
+document.querySelector('#new-post').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const body = e.target.body.value;
+    addPost({title, body});
+    e.target.reset();
+});
